4f4911a5df912cb24ae74b5b4907210976b7ad53
Cameron <cameron@moodycamel.com>
2016-03-23 18:00:29 -0400

    Another tweak to block alignment; same effect, but less memory wasted when elements are at least 16 bytes large

--- concurrentqueue.h
+++ concurrentqueue.h
@@ -1575,7 +1575,14 @@ private:
 		// should also align Block to the alignment of T in case it's higher than malloc's 16-byte
 		// alignment, but this is hard to do in a cross-platform way. Assert for this case:
 		static_assert(std::alignment_of<T>::value <= std::alignment_of<details::max_align_t>::value, "The queue does not support super-aligned types at this time");
-		char elements[sizeof(T) * BLOCK_SIZE];
+		// Additionally, we need the alignment of Block itself to be a multiple of max_align_t since
+		// otherwise the appropriate padding will not be added at the end of Block in order to make
+		// arrays of Blocks all be properly aligned (not just the first one). We use a union to force
+		// this.
+		union {
+			char elements[sizeof(T) * BLOCK_SIZE];
+			details::max_align_t dummy;
+		};
 	public:
 		Block* next;
 		std::atomic<size_t> elementsCompletelyDequeued;
@@ -1584,17 +1591,11 @@ private:
 		std::atomic<std::uint32_t> freeListRefs;
 		std::atomic<Block*> freeListNext;
 		std::atomic<bool> shouldBeOnFreeList;
+		bool dynamicallyAllocated;		// Perhaps a better name for this would be 'isNotPartOfInitialBlockPool'
 		
 #if MCDBGQ_TRACKMEM
 		void* owner;
 #endif
-		// Use a union with max_align_t on the last element in order to guarantee Blocks are a multiple of this size,
-		// so that arrays of Blocks are aligned to max_align_t. While this does waste a bit of memory, it is negligible
-		// compared to the size of the block as a whole (especially considering that blocks contain many elements each).
-		union {
-			bool dynamicallyAllocated;		// Perhaps a better name for this would be 'isNotPartOfInitialBlockPool'
-			details::max_align_t dummy;
-		};
 	};
     static_assert(std::alignment_of<Block>::value >= std::alignment_of<details::max_align_t>::value, "Internal error: Blocks must be at least as aligned as the type they are wrapping");
 
