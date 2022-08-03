258963cfb26ccf9159edb6ed37e4738b10666aa8
Innes MacKenzie <imackenzie@esri.com>
2022-08-03 13:35:34 +0100

    Add initial conanfile script

---
+++ conanfile_rtc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from conans import ConanFile
+
+
+class ConcurrentQueueConan(ConanFile):
+    name = "ConcurrentQueue"
+    description = " A fast multi-producer, multi-consumer lock-free concurrent queue "
+    topics = ("conan," "ConcurrentQueue", "lockfree", "queue")
+    url = "https://github.com/cameron314/concurrentqueue"
+    homepage = url
+    author = "Cameron <cameron@moodycamel.com>"
+    license = "Simplified BSD"
+    exports = ["LICENSE.md"]
+    exports_sources = ["src/*"]
+    no_copy_source = True
+
+    def package(self):
+        self.copy(pattern="LICENSE.md", dst="licenses")
+        self.copy(pattern="*", dst="include", src=".", keep_path=True)
+
+    def package_info(self):
+        if not self.in_local_cache:
+            self.cpp_info.includedirs = ["."]
+
+    def package_id(self):
+        self.info.header_only()
