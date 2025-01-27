0d54c6794510018f42b1b987f55c313dd1358320
Cameron <cameron@moodycamel.com>
2025-01-26 23:04:24 -0500

    Fix chain of happens-before between dequeue operations in a block and that block's reuse when empty, affecting implicit producers (see #404)

--- concurrentqueue.h
+++ concurrentqueue.h
@@ -1604,7 +1604,7 @@ private:
 			}
 			else {
 				// Increment counter
-				auto prevVal = elementsCompletelyDequeued.fetch_add(1, std::memory_order_release);
+				auto prevVal = elementsCompletelyDequeued.fetch_add(1, std::memory_order_acq_rel);
 				assert(prevVal < BLOCK_SIZE);
 				return prevVal == BLOCK_SIZE - 1;
 			}
@@ -1627,7 +1627,7 @@ private:
 			}
 			else {
 				// Increment counter
-				auto prevVal = elementsCompletelyDequeued.fetch_add(count, std::memory_order_release);
+				auto prevVal = elementsCompletelyDequeued.fetch_add(count, std::memory_order_acq_rel);
 				assert(prevVal + count <= BLOCK_SIZE);
 				return prevVal + count == BLOCK_SIZE;
 			}
