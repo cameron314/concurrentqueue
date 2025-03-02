2f09da73d22a47dc8a89cdd4fc4c3bfae07f4284
Cameron <cameron@moodycamel.com>
2025-03-01 20:15:08 -0500

    Fix warning (#408)

--- concurrentqueue.h
+++ concurrentqueue.h
@@ -509,9 +509,9 @@ namespace details
 	template<typename T>
 	static inline void swap_relaxed(std::atomic<T>& left, std::atomic<T>& right)
 	{
-		T temp = std::move(left.load(std::memory_order_relaxed));
-		left.store(std::move(right.load(std::memory_order_relaxed)), std::memory_order_relaxed);
-		right.store(std::move(temp), std::memory_order_relaxed);
+		T temp = left.load(std::memory_order_relaxed);
+		left.store(right.load(std::memory_order_relaxed), std::memory_order_relaxed);
+		right.store(temp, std::memory_order_relaxed);
 	}
 	
 	template<typename T>
